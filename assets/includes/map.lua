3266a67cd21fa37d91f2653a755c1dcd5a948f7f
Juan Latorre <juanlatorre@protonmail.com>
2018-06-06 01:28:31 -0400

    moved Map to his own class

--- assets/includes/map.lua
+++ assets/includes/map.lua
@@ -2,6 +2,16 @@
 
 Map = Object:extend()
 
-function Map.new(self)
-    self.test = "Mapa creado"
+sti = require("assets/lib/sti")
+
+function Map.new(self, rute)
+    self.map = sti(rute)
+end
+
+function Map.update(self, dt)
+    self.map:update(dt)
+end
+
+function Map.draw(self)
+    self.map:draw()
 end
